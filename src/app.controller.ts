fbcdf4fb4dbfd1e89e333e19d01ae6f60d142cc0
吴健泳 <3332189176@qq.com>
2018-06-27 17:04:26 +0800

    a

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -16,9 +16,27 @@ export class AppController {
     // console.log(333);
     return await this.appService.root()
   }
-  @Get('/user')
-  async user() {
-    return 11;
+  @Get('/getAccess_token')
+  async getAccess_token(@Res() R, @Query() params) {
+      const code = params.code;
+      // tslint:disable-next-line:max-line-length
+      https.get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxdd06f38bac305c95&secret=a202a88ea4b5ea8dbcc519af2997890e&code=${code}&grant_type=authorization_code`, (res) => {
+      console.log('statusCode:', res.statusCode);
+      console.log('headers:', res.headers);
+      const buffers = [];
+      res.on('data', (d) => {
+        process.stdout.write(d);
+        buffers.push(d);
+      });
+      res.on('end', (chunk) => {
+        const wholeData = Buffer.concat(buffers);
+        R.status(HttpStatus.OK).json(JSON.parse(wholeData.toString()));
+      });
+
+      }).on('error', (e) => {
+        console.error(e);
+        return e;
+      });
     // res.status(200).json('haha');
   }
   @Get('/getToken')
@@ -39,9 +57,9 @@ export class AppController {
       const sign = sha1.digest('hex');
       console.log(sign);
       if (sign === signature) {
-        return R.status(HttpStatus.OK).json(echostr);
+        return R.status(HttpStatus.OK).send(echostr);
       }
-      return R.status(HttpStatus.OK).json(false);
+      return R.status(HttpStatus.OK).send(false);
       }catch (err) {
         console.log(err); // 捕获错误
       }
@@ -66,4 +84,6 @@ export class AppController {
       // });
 
   }
+
+  
 }
