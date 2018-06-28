75f36eebe1961cd520a558ba8424aa639a498795
wujianyong1994 <463713231@qq.com>
2018-06-29 00:43:44 +0800

    a

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -63,7 +63,7 @@ export class AppController {
       const r = JSON.parse(request('GET', `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appid}&secret=${secret}&code=${code}&grant_type=authorization_code`).getBody().toString());
       if (r && r.openid) {
         //获取用户信息
-        const res = JSON.parse(request('GET', `https://api.weixin.qq.com/sns/userinfo?access_token=${r.access_token}&openid=${r.openid}`).getBody().toString());
+        const res = JSON.parse(request('GET', `https://api.weixin.qq.com/sns/userinfo?access_token=${r.access_token}&openid=${r.openid}&lang=zh_CN`).getBody().toString());
         return R.status(200).json(res)
       }
       return R.status(200).json({success:false,msg:'用户查询失败'});
